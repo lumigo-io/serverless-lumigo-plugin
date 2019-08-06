1ad47f1544e384d9791a00d11de7eef70f5548dc
Yan Cui <theburningmonk@gmail.com>
2019-08-06 07:22:25 +0100

    fix: bugs with v1.3.0 (#13)
    * fix: fix @lumigo/node-tracer is not installed
    
    * fix: fixed issue with handler not updated
    
    * style: fixed prettier issues
    
    * test: added test for verbose logging
    
    * style: fixed prettier errors

--- src/index.js
+++ src/index.js
@@ -44,7 +44,10 @@ class LumigoPlugin {
 			for (const func of functions) {
 				const handler = await this.createWrappedNodejsFunction(func, token);
 				// replace the function handler to the wrapped function
-				func.handler = handler;
+				this.verboseLog(
+					`setting [${func.localName}]'s handler to [${handler}]...`
+				);
+				this.serverless.service.functions[func.localName].handler = handler;
 			}
 		} else if (runtime === "python") {
 			await this.ensureLumigoPythonIsInstalled();
@@ -52,7 +55,10 @@ class LumigoPlugin {
 			for (const func of functions) {
 				const handler = await this.createWrappedPythonFunction(func, token);
 				// replace the function handler to the wrapped function
-				func.handler = handler;
+				this.verboseLog(
+					`setting [${func.localName}]'s handler to [${handler}]...`
+				);
+				this.serverless.service.functions[func.localName].handler = handler;
 			}
 		}
 
@@ -103,25 +109,25 @@ class LumigoPlugin {
 		try {
 			const packageJson = require(packageJsonPath);
 			const dependencies = _.get(packageJson, "dependencies", {});
-			return _.has(dependencies, "@lumigo/tracer");
+			return _.has(dependencies, "@lumigo/node-tracer");
 		} catch (err) {
 			this.verboseLog(
-				"error when trying to check if @lumigo/tracer is already installed..."
+				"error when trying to check if @lumigo/node-tracer is already installed..."
 			);
 			this.verboseLog(err.message);
-			this.verboseLog("assume @lumigo/tracer has not been installed...");
+			this.verboseLog("assume @lumigo/node-tracer has not been installed...");
 			return false;
 		}
 	}
 
 	async installLumigoNodejs() {
 		if (this.isNodeTracerInstalled) {
-			this.verboseLog("@lumigo/tracer is already installed, skipped...");
+			this.verboseLog("@lumigo/node-tracer is already installed, skipped...");
 			return;
 		}
 
-		this.log("installing @lumigo/tracer...");
-		await childProcess.execAsync("npm install @lumigo/tracer");
+		this.log("installing @lumigo/node-tracer...");
+		await childProcess.execAsync("npm install @lumigo/node-tracer");
 	}
 
 	async uninstallLumigoNodejs() {
@@ -129,8 +135,8 @@ class LumigoPlugin {
 			return;
 		}
 
-		this.log("uninstalling @lumigo/tracer...");
-		await childProcess.execAsync("npm uninstall @lumigo/tracer");
+		this.log("uninstalling @lumigo/node-tracer...");
+		await childProcess.execAsync("npm uninstall @lumigo/node-tracer");
 	}
 
 	async ensureLumigoPythonIsInstalled() {
--- src/index.test.js
+++ src/index.test.js
@@ -24,13 +24,15 @@ expect.extend({
 let serverless;
 let lumigo;
 
+const log = jest.fn();
+
 beforeEach(() => {
 	serverless = new Serverless();
 	serverless.servicePath = true;
 	serverless.service.service = "lumigo-test";
 	serverless.service.provider.compiledCloudFormationTemplate = { Resources: {} };
 	serverless.setProvider("aws", new AwsProvider(serverless));
-	serverless.cli = { log: jest.fn() };
+	serverless.cli = { log: log };
 	serverless.service.functions = {
 		hello: {
 			handler: "hello.world",
@@ -66,6 +68,8 @@ beforeEach(() => {
 	childProcess.exec.mockImplementation((cmd, cb) => cb());
 	const LumigoPlugin = require("./index");
 	lumigo = new LumigoPlugin(serverless, {});
+
+	delete process.env.SLS_DEBUG;
 });
 
 describe("Lumigo plugin (node.js)", () => {
@@ -123,6 +127,21 @@ describe("Lumigo plugin (node.js)", () => {
 				assertLumigoIsIncluded();
 			});
 		});
+
+		describe("if verbose logging is enabled", () => {
+			beforeEach(() => {
+				process.env.SLS_DEBUG = "*";
+			});
+
+			test("it should publish debug messages", async () => {
+				await lumigo.afterPackageInitialize();
+
+				const logs = log.mock.calls.map(x => x[0]);
+				expect(logs).toContain(
+					"serverless-lumigo: setting [hello]'s handler to [_lumigo/hello.world]..."
+				);
+			});
+		});
 	});
 
 	describe("nodejs10.x", () => {
@@ -177,6 +196,21 @@ describe("Lumigo plugin (node.js)", () => {
 
 				await lumigo.afterPackageInitialize();
 				assertLumigoIsIncluded();
+			});
+		});
+
+		describe("if verbose logging is enabled", () => {
+			beforeEach(() => {
+				process.env.SLS_DEBUG = "*";
+			});
+
+			test("it should publish debug messages", async () => {
+				await lumigo.afterPackageInitialize();
+
+				const logs = log.mock.calls.map(x => x[0]);
+				expect(logs).toContain(
+					"serverless-lumigo: setting [hello]'s handler to [_lumigo/hello.world]..."
+				);
 			});
 		});
 	});
@@ -378,6 +412,27 @@ lumigo_tracer`);
 				assertLumigoIsIncluded();
 			});
 		});
+
+		describe("if verbose logging is enabled", () => {
+			beforeEach(() => {
+				process.env.SLS_DEBUG = "*";
+			});
+
+			test("it should publish debug messages", async () => {
+				fs.pathExistsSync.mockReturnValue(true);
+				fs.readFile.mockReturnValue(`
+  --index-url https://1wmWND-GD5RPAwKgsdvb6DphXCj0vPLs@pypi.fury.io/lumigo/
+  --extra-index-url https://pypi.org/simple/
+  lumigo_tracer`);
+
+				await lumigo.afterPackageInitialize();
+
+				const logs = log.mock.calls.map(x => x[0]);
+				expect(logs).toContain(
+					"serverless-lumigo: setting [hello]'s handler to [_lumigo/hello.world]..."
+				);
+			});
+		});
 	});
 });
 
@@ -399,7 +454,7 @@ describe("is not nodejs or python", () => {
 
 function assertNodejsFunctionsAreWrapped() {
 	expect(childProcess.exec).toBeCalledWith(
-		"npm install @lumigo/tracer",
+		"npm install @lumigo/node-tracer",
 		expect.anything()
 	);
 
@@ -452,6 +507,14 @@ function assertNodejsFunctionsAreWrapped() {
 			`token: '${token}'`
 		)
 	);
+
+	const functions = serverless.service.functions;
+	expect(functions.hello.handler).toBe("_lumigo/hello.world");
+	expect(functions["hello.world"].handler).toBe("_lumigo/hello.world.handler");
+	expect(functions.foo.handler).toBe("_lumigo/foo.handler");
+	expect(functions.bar.handler).toBe("_lumigo/bar.handler");
+	expect(functions.jet.handler).toBe("_lumigo/jet.handler");
+	expect(functions.pack.handler).toBe("_lumigo/pack.handler");
 }
 
 function assertPythonFunctionsAreWrapped() {
@@ -504,6 +567,14 @@ function assertPythonFunctionsAreWrapped() {
 			`@lumigo_tracer(token='${token}')`
 		)
 	);
+
+	const functions = serverless.service.functions;
+	expect(functions.hello.handler).toBe("_lumigo/hello.world");
+	expect(functions["hello.world"].handler).toBe("_lumigo/hello.world.handler");
+	expect(functions.foo.handler).toBe("_lumigo/foo.handler");
+	expect(functions.bar.handler).toBe("_lumigo/bar.handler");
+	expect(functions.jet.handler).toBe("_lumigo/jet.handler");
+	expect(functions.pack.handler).toBe("_lumigo/pack.handler");
 }
 
 function assertFunctionsAreNotWrapped() {
@@ -514,7 +585,7 @@ function assertFunctionsAreNotWrapped() {
 function assertNodejsFunctionsAreCleanedUp() {
 	expect(fs.remove).toBeCalledWith(__dirname + "/_lumigo");
 	expect(childProcess.exec).toBeCalledWith(
-		"npm uninstall @lumigo/tracer",
+		"npm uninstall @lumigo/node-tracer",
 		expect.anything()
 	);
 }
