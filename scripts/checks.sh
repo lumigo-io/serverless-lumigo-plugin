1a88a02afc3bb39a391b358ecc8aee425c5981e8
Efi Merdler-Kravitz <43570637+efimk-lu@users.noreply.github.com>
2021-05-06 23:36:46 +0300

    Update checks.sh

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -18,19 +18,6 @@ sls invoke -l true -f test | grep "#LUMIGO#"
 sls remove
 popd
 
-pushd integration-test/nodejs-layers
-
-sls deploy
-
-echo "Results"
-sls invoke -l true -f test
-
-echo "Test"
-sls invoke -l true -f test | grep "#LUMIGO#"
-
-sls remove
-popd
-
 pushd integration-test
 rm -rf venv || true
 virtualenv venv -p python3.7
@@ -50,16 +37,6 @@ sls invoke -l true -f test-with-numbers
 echo "Test with numbers"
 sls invoke -l true -f test-with-numbers | grep "'type': 'function'"
 
-sls remove
-popd
-
-pushd integration-test/python-layers
-npm i
-sls deploy
-echo "Results"
-sls invoke -l true -f test
-echo "Test"
-sls invoke -l true -f test | grep "'type': 'function'"
 sls remove
 popd
 
