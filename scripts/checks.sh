9e6276d5b409c83291a874768374993cbe08ed4d
CircleCI <no-reply@build.com>
2022-07-14 16:50:28 +0300

    fix: adding pnpm tests

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -28,6 +28,28 @@ echo
 sls remove --stage $random
 popd
 
+pushd integration-test/nodejs-pnpm
+
+random=$RANDOM
+
+echo "** Testing NodeJS with PNPM **"
+echo "********************"
+echo
+echo
+
+echo "** Deploying **"
+echo
+sls deploy --force --stage $random
+
+echo "** Testing **"
+echo
+sls invoke -l -f test --stage $random | grep "#LUMIGO#"
+
+echo "** Removing stack **"
+echo
+sls remove --stage $random
+popd
+
 pushd integration-test/nodejs-esm
 
 random=$RANDOM
